1e83b066bf38e098dcaf170030b34d6c993b74c0
jae <dlworudgg@gmail.com>
2023-05-14 00:47:15 -0400

    Almost done with autocomplete of google map

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,6 +1,110 @@
+// import 'package:flutter/material.dart';
+// import 'package:siku/services/network_utility.dart';
+//
+// import '../components/location_list_tile.dart';
+// import '../models/autocomplete_prediction.dart';
+// import '../models/place_auto_complete_response.dart';
+// import '../theme.dart';
+// import 'home_screen.dart';
+//
+// class SearchScreen extends StatefulWidget {
+//   const SearchScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   State<SearchScreen> createState() => _SearchScreenState();
+// }
+//
+// class _SearchScreenState extends State<SearchScreen> {
+//
+//   List<AutocompletePrediction> placePredictions = [];
+//   Future<void> placeAutoComplete(String query) async {
+//     Uri uri = Uri.https(
+//        "maps.googleapis.com",
+//         'maps/api/place/autocomplete/json',
+//       {
+//         "input" : query,
+//         "key" : 'AIzaSyDIvQJfzX_91txHLSxwuPyzm-avQvGCYPo',
+//       }
+//     );
+//     String? response = await NetworkUtility.fetchUrl(uri);
+//     if (response!= null){
+//       PlaceAutocompleteResponse result =
+//           PlaceAutocompleteResponse.parseAutocompleteResult(response);
+//       if (result.predictions != null){
+//         setState(() {
+//           placePredictions = result.predictions!;
+//         });
+//       }
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: Stack(children: [
+//       Positioned(
+//         top: 60.0,
+//         left: 16.0,
+//         right: 16.0,
+//         child: TextFormField(
+//
+//           decoration: InputDecoration(
+//             hintText: 'Search here',
+//             fillColor: AppColors.cardLight,
+//             filled: true,
+//             prefixIcon: InkWell(
+//               onTap: () {
+//                 Navigator.pop(context);
+//                 // Navigator.push(
+//                   // context,
+//                   // MaterialPageRoute(builder: (context) => HomeScreen()),
+//                 // );
+//               },
+//               child: const Icon(Icons.arrow_back_ios),
+//             ),
+//
+//             border: OutlineInputBorder(
+//               borderSide: BorderSide.none,
+//               borderRadius: BorderRadius.circular(10),
+//             ),
+//           ),
+//           // textInputAction: TextInputAction.search,
+//         ),
+//       ),
+//       Positioned(
+//         top: 60.0,
+//         left: 16.0,
+//         right: 16.0,
+//         child: Column(
+//           Expanded(
+//             child: ListView.builder (
+//               itemCount: placePredictions.length,
+//               itemBuilder: (context, index) =>
+//                 LocationListTile(
+//                   press () {print("1")},
+//                   location : placePredictions[index].description!,
+//                 ),
+//             ),
+//           )
+//
+//           // const Divider(
+//           //   height:4,
+//           //   thickness: 4,
+//           //   color : Colors.grey[300].
+//           // ),
+//         ),
+//       ),
+//     ]
+//         )
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
+import 'package:siku/services/network_utility.dart';
 
 import '../components/location_list_tile.dart';
+import '../models/autocomplete_prediction.dart';
+import '../models/place_auto_complete_response.dart';
 import '../theme.dart';
 import 'home_screen.dart';
 
@@ -12,69 +116,75 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-
-  void placeAutoComplete(String query){
-    Uri url = Uri.https(
-       "maps.googleapis.com",
-        'maps/api/place/autocomplete/json',
+  List<AutocompletePrediction> placePredictions = [];
+  Future<void> placeAutoComplete(String query) async {
+    Uri uri = Uri.https(
+      "maps.googleapis.com",
+      'maps/api/place/autocomplete/json',
       {
-        "input" : query,
-        "key" : key
-      }
+        "input": query,
+        "key": 'AIzaSyDIvQJfzX_91txHLSxwuPyzm-avQvGCYPo',
+      },
     );
+    String? response = await NetworkUtility.fetchUrl(uri);
+    if (response != null) {
+      PlaceAutocompleteResponse result =
+      PlaceAutocompleteResponse.parseAutocompleteResult(response);
+      if (result.predictions != null) {
+        setState(() {
+          placePredictions = result.predictions!;
+        });
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(children: [
-      Positioned(
-        top: 60.0,
-        left: 16.0,
-        right: 16.0,
-        child: TextFormField(
-
-          decoration: InputDecoration(
-            hintText: 'Search here',
-            fillColor: AppColors.cardLight,
-            filled: true,
-            prefixIcon: InkWell(
-              onTap: () {
-                Navigator.pop(context);
-                // Navigator.push(
-                  // context,
-                  // MaterialPageRoute(builder: (context) => HomeScreen()),
-                // );
+      body: Stack(
+        children: [
+          Positioned(
+            top: 60.0,
+            left: 16.0,
+            right: 16.0,
+            child: TextFormField(
+              onChanged: (value) {
+                placeAutoComplete(value);
               },
-              child: const Icon(Icons.arrow_back_ios),
-            ),
-
-            border: OutlineInputBorder(
-              borderSide: BorderSide.none,
-              borderRadius: BorderRadius.circular(10),
+              decoration: InputDecoration(
+                hintText: 'Search here',
+                fillColor: AppColors.cardLight,
+                filled: true,
+                prefixIcon: InkWell(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Icon(Icons.arrow_back_ios),
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
             ),
           ),
-          // textInputAction: TextInputAction.search,
-        ),
-      ),
-      Positioned(
-        top: 60.0,
-        left: 16.0,
-        right: 16.0,
-        child: Column(
-          LocationListTile(
-            press () {},
-            location : ,
+          Positioned(
+            top: 100.0,
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: ListView.builder(
+              itemCount: placePredictions.length,
+              itemBuilder: (context, index) => LocationListTile(
+                press: () {
+                  print("1");
+                },
+                location: placePredictions[index].description!,
+              ),
+            ),
           ),
-          // const Divider(
-          //   height:4,
-          //   thickness: 4,
-          //   color : Colors.grey[300].
-          // ),
-        ),
+        ],
       ),
-    ]
-        )
     );
   }
 }
--- lib/services/network_utility.dart
+++ lib/services/network_utility.dart
@@ -1,16 +1,18 @@
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
-class NetworkUtility{
-static Future<String?> fetchUrl(Uri uri, {Map<String, String>? headers}) async {
-  try {
-    final response = await http.get(uri, headers: headers);
-    if (response.statusCode == 200) {
-      return response.body;
+class NetworkUtility {
+  static Future<String?> fetchUrl(Uri uri,
+      {Map<String, String>? headers}) async {
+    try {
+      final response = await http.get(uri, headers: headers);
+      if (response.statusCode == 200) {
+        return response.body;
+      }
     } catch (e) {
-    debugPrint(e.toString());
+      debugPrint(e.toString());
+    }
+    return null;
   }
-}
-retrun null;
-}
 
 }
