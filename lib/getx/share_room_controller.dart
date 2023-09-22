b3c5b88c1a2fbb7235be74747e4da012b6cdf514
jae <dlworudgg@gmail.com>
2023-09-21 23:38:29 -0400

    Created share_room_controller.dart

---
+++ lib/getx/share_room_controller.dart
@@ -0,0 +1,28 @@
+import 'package:get/get.dart';
+import 'package:contacts_service/contacts_service.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class ShareRoomController extends GetxController {
+  var contacts = <Contact>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _requestPermission();
+  }
+
+  Future<void> _requestPermission() async {
+    PermissionStatus status = await Permission.contacts.request();
+    if (status.isGranted) {
+      _loadContacts();
+    } else {
+      // Handle permission denied
+    }
+  }
+
+  Future<void> _loadContacts() async {
+    Iterable<Contact> contactList = await ContactsService.getContacts();
+    contacts.addAll(contactList);
+    // Apply your logic to filter contacts who are using your app
+  }
+}
